d8d910a8ed61f6028af7ec77456aef20e2f5b820
Cristofer Casas <cristofer32513@gmail.com>
2018-11-14 23:20:49 -0600

    Se agrego el metodo ordenamientoBurbuja0.

--- src/EjemploMetodosOrdenamiento.java
+++ src/EjemploMetodosOrdenamiento.java
@@ -1,5 +1,19 @@
 class MetodosDeOrdenamiento{
 
+	
+	public void deBurbuja0(int[] datos){
+		int aux=0;
+		for(int i=0;i<datos.length;i++){
+			for(int j=(i+1);j<datos.length;j++){
+				if(datos[i]>datos[(j)]){
+					aux=datos[i];
+					datos[i]=datos[j];
+					datos[j]=aux;
+				}
+			}
+		}
+	}
+	
 }
 
 public class EjemploMetodosOrdenamiento {
