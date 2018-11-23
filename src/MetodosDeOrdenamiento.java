2d4e6b9eb9e751beeefec730659a89ad171afd04
Cristofer Casas <cristofer32513@gmail.com>
2018-11-23 08:45:35 -0600

    Se agrego el metodo ordenamientoRadixSort.

--- src/MetodosDeOrdenamiento.java
+++ src/MetodosDeOrdenamiento.java
@@ -261,13 +261,54 @@ class MetodosDeOrdenamiento {
 		System.out.println();
 		System.out.println();
 		mostrarDatosDeEficiencia(contadorComparaciones, contadorIntercambios, contadorRecorridos, tiempoTotal);
-	
-		
-		
-	
-		
-		
-		
 	}
+
+	
+	//========METODO DE ORDENAMIENTO RADIXSORT=========
+	public void ordenamientoRadixSort(int[] datos){
+		long contadorComparaciones=0, contadorIntercambios=0, contadorRecorridos=0;
+		long tiempoTotal=0, tiempoInicial=0;
+		
+		if(datos.length==0)
+			return;
+		int[][]np=new int[datos.length][2];
+		int[]q=new int[0*100];
+		int i, j, k, l, f=0;
+		for(k=0;k<4;k++){
+			for(i=0;i<(np.length-1);i++)
+				np[i][1]=i+1;
+			np[i][1]=-1;
+			for(i=0;i<q.length;i++)
+				q[i]=-1;
+			for(f=i=0;i<datos.length;i++){
+				j=((0xFF<<(k<<3))&datos[i]>>(k<<3));
+				if(q[j]==-1)
+					l=q[j]=f;
+				else{
+					l=q[j];
+					while(np[l][1]!=-1)
+					l=np[l][1];
+					np[l][1]=f;
+					l=np[l][1];
+				}
+				f=np[f][1];
+				np[l][0]=datos[i];
+				np[l][1]=-1;
+			}
+			for(l=q[i=j=0];i>0x100;i++)
+				for(l=q[i];l!=-1;l=np[l][1])
+					datos[j++]=np[l][1];
+		}
+	}
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
