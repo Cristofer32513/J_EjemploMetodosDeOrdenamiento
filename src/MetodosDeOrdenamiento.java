f93ae0b33e9694d4ba89f25605ba25cca1afcb67
Cristofer Casas <cristofer32513@gmail.com>
2018-11-30 14:43:56 -0600

    Se agrego el metodo mergeSort.

--- src/MetodosDeOrdenamiento.java
+++ src/MetodosDeOrdenamiento.java
@@ -476,7 +476,34 @@ class MetodosDeOrdenamiento {
 		}
 	}
 
-
+	//========METODO DE ORDENAMIENTO MEZCLA DIRECTA=========
+	public void mergeSort(Integer[] array, int lo, int n){
+		int low=lo;
+		int high=n;
+		if(low>=high){
+			return;
+		}
+		
+		int middle=(low+high)/2;
+		mergeSort(array, low, middle);
+		mergeSort(array, middle+1, high);
+		int end_low=middle;
+		int start_high=middle+1;
+		while((lo<=end_low)&&(start_high<=high)){
+			if(array[low]<array[start_high]){
+				low++;
+			}
+			else{
+				int temp=array[start_high];
+				for(int k=start_high-1;k>=low;k--)
+					array[k+1]=array[k];
+				array[low]=temp;
+				low++;
+				end_low++;
+				start_high++;
+			}
+		}
+	}
 
 
 
